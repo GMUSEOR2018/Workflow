4b6fa955885b6ed0cb5f0f90695636e00ab398e6
Yuan <yaoyuan.william@gmail.com>
2018-10-24 16:58:07 -0400

    Engineer

--- src/simulation/Engineer.java
+++ src/simulation/Engineer.java
@@ -1,13 +1,18 @@
 package simulation;
-
+//Engineer for DCB
 public class Engineer extends Employee {
-
+//Construction
 	public Engineer(int ID, int suporisvorID, String name) {
-		// TODO Auto-generated constructor stub
 		this.ID=ID;
 		this.supervisorID=suporisvorID;
 		this.name=name;
-		this.Shift=simulation.Shift.Day;
+		this.Shift=Shift.Day;
 	}
-
+	public void Assign(WorkOrder WO) {
+		this.WO=WO;
+		makePlan();
+	}
+	private void makePlan() {
+		WO.setPlan();
+	}
 }
