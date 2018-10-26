ec5107da2f4d2050766cc6a7fbba8623ffdf6461
Yuan <yaoyuan.william@gmail.com>
2018-10-26 11:15:00 -0400

    New V

--- src/simulation/Current.java
+++ src/simulation/Current.java
@@ -1,9 +1,14 @@
 package simulation;
 
+import java.sql.Date;
+
 public class Current {
 
 	public Current() {
 		// TODO Auto-generated constructor stub
+		Engineer E= new Engineer(1,2,"E");
+		
+		E.Assign(new WorkOrder(1,Types.SHINV,Location.NE,Date.from(null)));
 	}
 
 }
--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -7,10 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello!");;
-		
-		Engineer E= new Engineer(1,2,"E");
-		
-		E.Assign(new WorkOrder(1,Types.SHNOT,Location.NE,Date.from(null)));
+		Current c= new Current();
 	}
 
 }
