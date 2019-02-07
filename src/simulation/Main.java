6d71ecab69e0c1e9e0f1a4a3a4cd004eeecdc9dc
Yuan <yaoyuan.william@gmail.com>
2019-02-07 09:00:59 -0500

    bug fix

--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -4,9 +4,12 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-	generateWO g = new generateWO(100);
-	g.run();
+	//generateWO g = new generateWO(10000);
+	//g.run();
+	Current c= new Current();
+	c.run();
 	System.out.print("Done!!");
+
 	}
 
 }
