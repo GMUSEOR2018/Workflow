7ef70d3ac0c19fe39a4cf1afd68e7d177af5bde8
Yuan <yaoyuan.william@gmail.com>
2019-02-17 12:37:24 -0500

    v1.2

--- src/simulation/Main.java
+++ src/simulation/Main.java
@@ -1,12 +1,14 @@
 package simulation;
 import java.io.IOException;
 
-
 public class Main {
 	public static void main(String[] args) throws IOException, CloneNotSupportedException {
+		int replication=1000;
+		long t = System.currentTimeMillis(); // start time record.
 		System.out.println("Start!");
-		Run r =new Run(10);
+		Run r =new Run(replication);
 		r.sim();
-		System.out.println("Done!");
+		r.Backlog();
+		System.out.print("\n"+replication+" runs done, operation took: " + (1.0*(System.currentTimeMillis()-t)/1000) + " seconds to complete.\n");
 	}
 }
